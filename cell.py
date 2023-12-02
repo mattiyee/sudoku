b7b7f7ce06ea5c1141b77b8ba5b8946e136e590d
prestonhemmy <phemmy@ufl.edu>
2023-12-02 15:17:05 -0500

    svdsv

--- cell.py
+++ cell.py
@@ -1,5 +1,9 @@
 import pygame
 
+black = (0, 0, 0)
+c_width, c_height = 600 / 9, 600 / 9
+
+
 class Cell:
     # Constructor for the Cell class
     def __init__(self, value, row, col, screen):
@@ -8,6 +12,7 @@ class Cell:
         self.row = row
         self.col = col
         self.screen = screen
+        self.bool = False
 
     # Function that sets the cell’s value
     def set_cell_value(self, value):
@@ -17,14 +22,20 @@ class Cell:
     def set_sketched_value(self, value):
         self.sketched_value = value
 
+    def set(self):
+        pass
+
     # Function that draws the cell, along with the value inside it
     def draw(self):
-        cell_font = pygame.font.Font(None, 100)  # Change the size
-
-        '''
+        num_font = pygame.font.Font(None, 70)  # Change the size
         if self.value != 0:
-            cell_surface
-        '''
+            num_surf = num_font.render(str(self.value), 0, black)
+            num_rect = num_surf.get_rect(
+                center=(c_height * self.row + c_height // 2, c_width * self.col + c_width // 2))
+            self.screen.blit(num_surf, num_rect)
+        if self.bool:
+            pygame.draw.rect(self.screen, black,
+                             pygame.rect(self.row * c_height, self.col * c_width, c_height, c_width, 3))
 
     # If this cell has a nonzero value, that value is displayed.
     # Otherwise, no value is displayed in the cell.
---
+++ tmp.py
@@ -0,0 +1,6 @@
+from cell import Cell
+
+test_board = [[]*9]*9
+test_cell = Cell(3, 4, 4, test_board)
+
+test_cell.draw()
